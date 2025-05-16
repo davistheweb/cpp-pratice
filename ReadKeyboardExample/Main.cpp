7a95b9bfdb48ce513a380c8cd308b0d1fe7f53d1
davistheweb <josiahdave001@gmail.com>
2025-05-16 04:10:59 +0100

    :

---
+++ ReadKeyboardExample/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    cout << "Enter a number for variable x : ";
+    cin >> x;
+    cout << "Enter a number for variable y : ";
+    cin >> y;
+
+    cout << "There sum is " << x+y << endl;
+}
